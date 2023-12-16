f3c77e238b15d85e0da0a7fd658d16dd03cb2115
arthdarji352 <arthdarji20@gmail.com>
2023-12-16 14:05:12 +0530

    make utils

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 // require("dotenv").config({path:"./env"})
@@ -6,7 +6,13 @@ import dotenv from "dotenv";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port :${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("mongoDB connection failed!!!", err));
 
 /*
 IIFE approach
